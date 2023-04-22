1a87da80d4992a9b20164951501395488ce2bb0c
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-04-21 20:58:44 -0400

    Primera Prueba Solemne C# - Soluciones en Consola

---
+++ ConsolePAES/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ConsolePAES
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            //Declaración de Variables
+            String? strNumLocales, strCantidadPersonas, strCantidadPersonasSobre700;
+            double numLocales=0, contador=1, cantidadPersonas=0, cantidadPersonasSobre700=0, totalGlobalPersonas=0, totalSobre700=0, porcentajeSobre700=0, nivelLocal=0, nivel1=0, nivel2=0, nivel3=0, nivel4=0;
+
+            //Input Número de Locales (Cantidad de ciclos)
+            Console.WriteLine("Ingrese el número de locales a registrar: ");
+            strNumLocales = Console.ReadLine();
+            double.TryParse(strNumLocales, out numLocales);
+
+            //Ciclo While de Almacenaje de Datos
+            while(contador <= numLocales)
+            {
+                //Input y Conversion de Variables
+                Console.WriteLine("Nombre del Local:");
+                Console.ReadLine();
+                Console.WriteLine("Cantidad de Personas que rindieron PAES:");
+                strCantidadPersonas = Console.ReadLine();
+                double.TryParse(strCantidadPersonas,out cantidadPersonas);
+                Console.WriteLine("Cantidad de Personas con más de 700 puntos:");
+                strCantidadPersonasSobre700 = Console.ReadLine();
+                double.TryParse(strCantidadPersonasSobre700, out cantidadPersonasSobre700);
+
+                //Cálculo de Nivel Local
+                nivelLocal = cantidadPersonasSobre700 * 100 / cantidadPersonas;
+
+                //Clasificación de Niveles
+                if(nivelLocal >= 80)
+                {
+                    nivel1++;
+                }
+                if(nivelLocal <= 80 && nivelLocal >=60)
+                {
+                    nivel2++;
+                }
+                if(nivelLocal <= 60 && nivelLocal >= 40)
+                {
+                    nivel3++;
+                }
+                if(nivelLocal <= 40 && nivelLocal >= 20)
+                {
+                    nivel4++;
+                }
+
+                //Cálculo de Total Global de Personas y Porcentaje de Personas sobre 700 puntos
+                totalGlobalPersonas = totalGlobalPersonas + cantidadPersonas;
+                totalSobre700 = totalSobre700 + cantidadPersonasSobre700;
+                porcentajeSobre700 = totalSobre700 * 100 / totalGlobalPersonas;
+
+                contador++;
+            }
+            //Outputs
+            Console.WriteLine($"\nLa Cantidad Total de Personas que rindieron PAES en los {numLocales} Locales es de: {totalGlobalPersonas}");
+            Console.WriteLine($"La Cantidad Total de Personas que obtubieron un puntajes sobre 700 es de: {totalSobre700}");
+            Console.WriteLine($"El Porcentaje Total de Personas que obtuvieron un puntaje sobre 700 es del: {porcentajeSobre700}%");
+            Console.WriteLine($"\nCantidad de Locales con Nivel 1 - Excelencia: {nivel1}");
+            Console.WriteLine($"Cantidad de Locales con Nivel 2 - Bueno: {nivel2}");
+            Console.WriteLine($"Cantidad de Locales con Nivel 3 - Regular: {nivel3}");
+            Console.WriteLine($"Cantidad de Locales con Nivel 4 - Inicial: {nivel4}");     
+        }
+    }
+}
---
+++ ConsoleSartekComp/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace ConsoleSartekComp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Declaración e Inicio de Variables
+            String menu = "\n\tSARTEKCOMP \n \n 1.- Venta de Productos\n 2.- Total a Pagar\n 3.- Estadísticas\n 4.- Salir\n";
+            int opcion = 0, edad = 0, cantidad = 0, contadorNotebook = 0, contadorImpresora = 0, contadorMicAudifonos = 0;
+            String? strOpcion = "", strEdad = "", strTipoProducto = "", strCantidad = "", strMetodoPago = "";
+            double valorNotebook, valorImpresora, valorMicAudifonos, descuentoCantidad, descuentoEdad, descuentoTransferencia, totalDescuento = 0, totalPagar = 0;
+
+            valorNotebook = 350000;
+            valorImpresora = 150000;
+            valorMicAudifonos = 50000;
+
+            descuentoCantidad = 0.1;
+            descuentoEdad = 0.2;
+            descuentoTransferencia = 0.05;
+
+            do
+            {
+                //Despliegue Menú
+                Console.WriteLine(menu + "\n" + "Ingrese una Opción...");
+                strOpcion = Console.ReadLine();
+                int.TryParse(strOpcion,out opcion);
+                
+                //Selección del Menú de Opciones
+                switch (opcion)
+                {
+                    //Inputs Sección "Venta de Productos"
+                    case 1:
+                    Console.WriteLine("Ingrese su Edad: ");
+                    strEdad = Console.ReadLine();
+                    int.TryParse(strEdad, out edad);
+                    Console.WriteLine("Ingrese el Tipo de Producto:\n \n N - Notebook\n I - Impresora Láser\n MA - Juego de Micrófono Audífonos");
+                    strTipoProducto = Console.ReadLine();
+                    Console.WriteLine("Ingrese la cantidad de Productos que desea comprar: ");
+                    strCantidad = Console.ReadLine();
+                    int.TryParse(strCantidad, out cantidad);
+                    break;
+                    
+                    case 2:
+                    //Sentencias para Clasificación de Tipo de Producto, Cálculo Total a Pagar y Aplicación de Descuentos según Edad, Cantidad y Método de Pago            
+                    if(strTipoProducto == "N")
+                    {
+                        totalPagar = valorNotebook * cantidad;
+                        contadorNotebook = contadorNotebook + cantidad;
+                    }
+                    if(strTipoProducto == "I")
+                    {
+                        totalPagar = valorImpresora * cantidad;
+                        contadorImpresora = contadorImpresora + cantidad;                     
+                    }
+                    if(strTipoProducto == "MA")
+                    {
+                        totalPagar = valorMicAudifonos * cantidad;
+                        contadorMicAudifonos = contadorMicAudifonos + cantidad;
+                    }
+
+                    if(edad >= 40)
+                    {
+                        totalDescuento = totalPagar * descuentoEdad;
+                        totalPagar = totalPagar - totalDescuento;
+                    }
+                    if(cantidad >= 4)
+                    {
+                        totalDescuento = totalPagar * descuentoCantidad;
+                        totalPagar = totalPagar - totalDescuento;
+                    }
+
+                    //Selección de Método de Pago
+                    Console.WriteLine($"Seleccione el método de Pago: \n D - Débito\n C - Crédito\n T - Transferencia");
+                    strMetodoPago = Console.ReadLine();
+                    if(strMetodoPago == "T")
+                    {
+                        totalDescuento = totalPagar * descuentoTransferencia;
+                        totalPagar = totalPagar - totalDescuento;
+                        Console.WriteLine($"Total a Pagar: {totalPagar}");
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"Total a Pagar: {totalPagar}");
+                    }
+
+                    break;
+                    case 3:
+                    //Outputs de Estadísticas de Ventas
+                    Console.WriteLine($"Total de Notebooks Vendidos: {contadorNotebook}");
+                    Console.WriteLine($"Total de Impresoras Vendidas: {contadorImpresora}");
+                    Console.WriteLine($"Total de Juegos Micrófono-Audífonos Vendidos: {contadorMicAudifonos}");
+                    break;
+                    case 4:
+                    //Salida de la Aplicación
+                    Console.WriteLine("Saliendo de la aplicación...");
+                    break;
+                    default:
+                    //Validación Default para Opción No Válida
+                    Console.WriteLine("Opción No Válida, Intente Nuevamente...");
+                    break;
+                }
+
+            }while(opcion != 4);
+        }
+    }
+}
